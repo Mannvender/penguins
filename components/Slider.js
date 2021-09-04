ee28fdd29850deda3c60ced550a1455e6ff47509
mannvender <mannvender001@gmail.com>
2021-09-04 09:55:07 +0530

    added more penguins

--- components/Slider.js
+++ components/Slider.js
@@ -1,9 +1,47 @@
 import React from "react";
 import styled from "styled-components";
 import Image from "next/image";
+import { Box } from "rebass";
 import { isMobile } from "react-device-detect";
 
-const URLS = ["001", "002", "003", "001", "002", "003", "001", "002", "003"];
+const URLS = [
+  "004",
+  "005",
+  "006",
+  "007",
+  "008",
+  "009",
+  "010",
+  "011",
+  "012",
+  "013",
+  "014",
+  "015",
+  "016",
+  "017",
+  "018",
+  "019",
+  "020",
+  "021",
+  "022",
+  "023",
+  "024",
+  "025",
+  "026",
+  "027",
+  "028",
+  "029",
+  "030",
+  "031",
+  "032",
+  "033",
+  "034",
+  "035",
+  "036",
+  "037",
+  "038",
+  "039",
+];
 const Container = styled.div`
   position: relative;
   overflow: hidden;
@@ -29,13 +67,36 @@ const AnimatedBg = styled.div`
   }
 `;
 const AnimatedPenguins = styled.div`
-  bottom: 6.9%;
+  height: 52%;
+  bottom: 18%;
   width: max-content;
   transform: translate3d(0, 0, 0);
   animation: slideshow 25s linear infinite;
   @keyframes slideshow {
     100% {
       transform: translateX(-66.6%);
+    }
+  }
+  @media (min-width: 1024px) {
+    bottom: 31%;
+  }
+  @media (min-width: 1281px) {
+    bottom: 22%;
+  }
+`;
+const AnimatedFood = styled.div`
+  bottom: 7%;
+  width: 100vw;
+  height: 40%;
+  background-image: url("/food.png");
+  background-size: auto 100%;
+  animation: animated-section-move 3.07s linear infinite;
+  @keyframes animated-section-move {
+    from {
+      background-position-x: 0;
+    }
+    to {
+      background-position-x: calc(100% - 100vw);
     }
   }
 `;
@@ -55,6 +116,7 @@ const Slider = () => {
           />
         ))}
       </AnimatedPenguins>
+      <AnimatedFood className="inner" />
     </Container>
   );
 };
