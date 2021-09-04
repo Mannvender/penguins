4e4feec93172c390ed744473063920f020ddc42a
mannvender <mannvender001@gmail.com>
2021-09-04 13:01:03 +0530

    added react spring animation in team section

--- components/homepage/Team.js
+++ components/homepage/Team.js
@@ -2,13 +2,33 @@ import React from "react";
 import { Heading, Box, Flex, Text } from "rebass";
 import styled, { useTheme } from "styled-components";
 import Image from "next/image";
+import { useSprings, animated } from "react-spring";
 
 const StyledBox = styled(Box)`
   border-bottom: 8px solid ${(props) => props.theme.colors[props.borderColor]};
   position: relative;
+  &:hover {
+    img {
+      display: none;
+    }
+  }
 `;
+const calc = (x, y) => [
+  -(y - window.innerHeight / 2) / 20,
+  (x - window.innerWidth / 2) / 20,
+  1.1,
+];
+const trans = (x, y, s) =>
+  `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`;
+
 const TeamSection = ({ team }) => {
   const { colors } = useTheme();
+  const [props, set] = useSprings(4, () => ({
+    xys: [0, 0, 1],
+    opacity: 1,
+    config: { mass: 5, tension: 350, friction: 40 },
+  }));
+  console.log(props);
   return (
     <Flex
       flexDirection="column"
@@ -41,57 +61,56 @@ const TeamSection = ({ team }) => {
                 </Text>
                 <Text fontSize={[1, 2]}>{member.description}</Text>
               </Box>
-              <StyledBox
-                key={index}
-                p={[2]}
-                pb={0}
-                borderColor={member.color}
-                backgroundColor={colors.light2}
+              <animated.div
+                onMouseMove={({ clientX: x, clientY: y }) =>
+                  set((i) => {
+                    if (i === index) return { xys: calc(x, y), opacity: 0 };
+                  })
+                }
+                onMouseLeave={() =>
+                  set((i) => {
+                    if (i === index) return { xys: [0, 0, 1], opacity: 1 };
+                  })
+                }
+                style={{ transform: props[index].xys.interpolate(trans) }}
               >
-                {true && (
-                  <Box
-                    sx={{
-                      position: "absolute",
-                      top: ["-10px"],
-                      left: ["8px"],
-                    }}
-                    height={["80px"]}
-                    width={["50px"]}
-                  >
-                    <Image
-                      layout="fill"
-                      src={"/pin_02.png"}
-                      alt={`band-aid`}
-                      quality="70"
-                    />
-                  </Box>
-                )}
-                <Image
-                  height="300px"
-                  width="300px"
-                  src={member.image.src}
-                  alt={`scholar penguin`}
-                  quality="70"
-                />
-                {false && (
-                  <Box
-                    sx={{
-                      position: "absolute",
-                      bottom: ["-20px"],
-                      left: ["200px"],
-                    }}
-                    height={["80px"]}
-                    width={["80px"]}
-                  >
-                    <Image
-                      layout="fill"
-                      src={"/bandaid.png"}
-                      alt={`band-aid`}
-                      quality="70"
-                    />
-                  </Box>
-                )}
-              </StyledBox>
+                <StyledBox
+                  key={index}
+                  p={[2]}
+                  pb={0}
+                  borderColor={member.color}
+                  backgroundColor={colors.light2}
+                >
+                  {true && (
+                    <animated.div style={{ opacity: props[index].opacity }}>
+                      <Box
+                        sx={{
+                          position: "absolute",
+                          top: ["-10px"],
+                          left: ["8px"],
+                        }}
+                        style={{}}
+                        height={["80px"]}
+                        width={["50px"]}
+                      >
+                        <Image
+                          layout="fill"
+                          src={"/pin_02.png"}
+                          alt={`band-aid`}
+                          quality="70"
+                        />
+                      </Box>
+                    </animated.div>
+                  )}
+                  <Image
+                    height="300px"
+                    width="300px"
+                    src={member.image.src}
+                    alt={`scholar penguin`}
+                    quality="70"
+                  />
+                </StyledBox>
+              </animated.div>
             </Box>
           ))}
         </Flex>
