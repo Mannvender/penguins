611a3298677399487a0beaef302e337db5ac89e5
mannvender <mannvender001@gmail.com>
2021-10-10 20:01:45 +0530

    updated timer

--- messages.js
+++ messages.js
@@ -1,181 +1,174 @@
 export const DEFAULT_ERROR_MESSAGE =
-  'Something went wrong! We are looking into it.'
+  "Something went wrong! We are looking into it.";
 
 export const ROADMAP = [
-  { when: '10', what: '10 people get Baskin Robbins.', icebergWidth: '36%' },
-  { when: '20', what: 'Icedrops.', icebergWidth: '45%' },
+  { when: "10", what: "10 people get Baskin Robbins.", icebergWidth: "36%" },
+  { when: "20", what: "Icedrops.", icebergWidth: "45%" },
   {
-    when: '40',
-    what:
-      "Sue people who minted yetis cunning foxes & chill frogs but didn't mint us.",
-    icebergWidth: '100%'
+    when: "40",
+    what: "Sue people who minted yetis cunning foxes & chill frogs but didn't mint us.",
+    icebergWidth: "100%",
   },
   {
-    when: '60',
-    what: 'Collab with other projects.',
-    icebergWidth: '63%'
+    when: "60",
+    what: "Collab with other projects.",
+    icebergWidth: "63%",
   },
   {
-    when: '80',
-    what: 'Icedrops, keeps on melting.',
-    icebergWidth: '65%'
+    when: "80",
+    what: "Icedrops, keeps on melting.",
+    icebergWidth: "65%",
   },
   {
-    when: '100',
-    what: 'Begin our journey to 1ETH',
-    icebergWidth: '70%'
-  }
-]
+    when: "100",
+    what: "Begin our journey to 1ETH",
+    icebergWidth: "70%",
+  },
+];
 export const DADDY_ROADMAP = [
   {
-    what:
-      'merch - hoodies with penguin art & stuff - not just T shirts with prints but hoodies that have penguin mouth as cap, some gourmet shit',
-    tag: 'merch'
+    what: "merch - hoodies with penguin art & stuff - not just T shirts with prints but hoodies that have penguin mouth as cap, some gourmet shit",
+    tag: "merch",
   },
   {
-    what: 'jpeg backed by jpeg - companion fish',
-    tag: 'jpg'
+    what: "jpeg backed by jpeg - companion fish",
+    tag: "jpg",
   },
   {
-    what:
-      'well scratch that - life advice - life lessons from penguins that survived 2 extinction events - as a jpeg - like the insta posts but better',
-    tag: 'life'
+    what: "well scratch that - life advice - life lessons from penguins that survived 2 extinction events - as a jpeg - like the insta posts but better",
+    tag: "life",
   },
   {
-    what: 'comics',
-    tag: 'book'
+    what: "comics",
+    tag: "book",
   },
   {
-    what:
-      'well scratch that too - what are we nerds - these polars look like they could star in a suicide squad spinoff',
-    tag: 'dc'
+    what: "well scratch that too - what are we nerds - these polars look like they could star in a suicide squad spinoff",
+    tag: "dc",
   },
   {
-    what:
-      'climate change - not just buying senators & bribing politicians but changing the mood of the public',
-    tag: 'cloud'
+    what: "climate change - not just buying senators & bribing politicians but changing the mood of the public",
+    tag: "cloud",
   },
   {
-    what:
-      "bollywood movie - well technically an indie movie in India but it'll have dances mid scene, an overly ruffled plot & broken physics",
-    tag: 'bollywood'
+    what: "bollywood movie - well technically an indie movie in India but it'll have dances mid scene, an overly ruffled plot & broken physics",
+    tag: "bollywood",
   },
   {
     what: `a penguin convertible car - read
     <a target='_blank' rel='noopener noreferrer' href='https://www.scientificamerican.com/article/how-to-prevent-air-conditioners-from-heating-the-planet/' class='underlined-link'>this article</a>
     on why AC exacerbates climate change issue`,
-    tag: 'car'
+    tag: "car",
   },
   {
     what: "sue the guys who didn't mint us again for good measure",
-    tag: 'hammer'
+    tag: "hammer",
   },
   {
-    what:
-      'think of more ways to make $s & make the floor go through the roof - idk 1/2 a million$s gives you million ways to do awesome things',
-    tag: 'money'
-  }
-]
+    what: "think of more ways to make $s & make the floor go through the roof - idk 1/2 a million$s gives you million ways to do awesome things",
+    tag: "money",
+  },
+];
 
-export const MINT_DATE = new Date('2021-10-10T16:00:00.000+00:00')
+export const MINT_DATE = new Date("2021-10-12T16:00:00.000+00:00");
 export const DATE_OPTIONS = {
-  weekday: 'long',
-  year: 'numeric',
-  month: 'long',
-  day: 'numeric'
-}
-export const IS_MINT_LINK_VISIBLE = false
+  weekday: "long",
+  year: "numeric",
+  month: "long",
+  day: "numeric",
+};
+export const IS_MINT_LINK_VISIBLE = false;
 
 export const FEATURES = [
   {
-    title: '3000',
-    text: 'polar penguins'
+    title: "3000",
+    text: "polar penguins",
   },
   {
-    title: '0.05 ETH',
-    text: '+ that old hag gas'
+    title: "0.05 ETH",
+    text: "+ that old hag gas",
   },
   {
-    title: '100+',
-    text: 'traits'
+    title: "100+",
+    text: "traits",
   },
   {
-    title: 'IPFS',
-    text: 'storage'
-  }
-]
+    title: "IPFS",
+    text: "storage",
+  },
+];
 
 export const LINKS = [
-  { href: '/#penguins', label: 'Penguins', showOnMobile: true },
-  { href: '/#stats', label: 'Stats', showOnMobile: false },
-  { href: '/#team', label: 'Team', showOnMobile: false },
-  { href: '/#roadmap', label: 'Roadmap', showOnMobile: true },
-  { href: '/#faqs', label: 'FAQs', showOnMobile: true },
-  { href: '/#launch', label: 'Launch', showOnMobile: true }
-]
+  { href: "/#penguins", label: "Penguins", showOnMobile: true },
+  { href: "/#stats", label: "Stats", showOnMobile: false },
+  { href: "/#team", label: "Team", showOnMobile: false },
+  { href: "/#roadmap", label: "Roadmap", showOnMobile: true },
+  { href: "/#faqs", label: "FAQs", showOnMobile: true },
+  { href: "/#launch", label: "Launch", showOnMobile: true },
+];
 
 export const TEAM = [
   {
-    name: 'Scholar Penguin',
-    description: 'Drinks n knows a thing or 2 about nfts',
-    color: 'light',
-    image: { src: '/penguin_bg_03.jpeg', bgColor: '#5cffe4' }
+    name: "Scholar Penguin",
+    description: "Drinks n knows a thing or 2 about nfts",
+    color: "light",
+    image: { src: "/penguin_bg_03.jpeg", bgColor: "#5cffe4" },
   },
   {
-    name: 'Roller Penguin',
-    description: 'Its a hot hot world, our contact to the outside icebergs',
-    color: 'light',
-    image: { src: '/penguin_bg_04.jpeg', bgColor: '#ff5df1' }
+    name: "Roller Penguin",
+    description: "Its a hot hot world, our contact to the outside icebergs",
+    color: "light",
+    image: { src: "/penguin_bg_04.jpeg", bgColor: "#ff5df1" },
   },
   {
-    name: 'Dollar Penguin',
-    description: 'Developer of the www world, walker of the night',
-    color: 'light',
-    image: { src: '/penguin_bg_01.jpeg', bgColor: '#000' }
+    name: "Dollar Penguin",
+    description: "Developer of the www world, walker of the night",
+    color: "light",
+    image: { src: "/penguin_bg_01.jpeg", bgColor: "#000" },
   },
   {
-    name: 'Painter Penguin',
+    name: "Painter Penguin",
     description:
-      'Picasso amongst us mortals - humble, hardworkin & honest - 2 lies 1 truth',
-    color: 'light',
-    image: { src: '/penguin_bg_02.jpeg', bgColor: '#ae0000' }
-  }
-]
+      "Picasso amongst us mortals - humble, hardworkin & honest - 2 lies 1 truth",
+    color: "light",
+    image: { src: "/penguin_bg_02.jpeg", bgColor: "#ae0000" },
+  },
+];
 
 export const FAQS = [
   {
-    question: 'When mint?',
-    answer: 'Does 19th work for you, it works for us',
-    height: ['56px', '40px']
+    question: "When mint?",
+    answer: "Does 19th work for you, it works for us",
+    height: ["56px", "40px"],
   },
   {
-    question: 'Where mint?',
-    answer: 'On this site dude comeon',
-    height: ['40px']
+    question: "Where mint?",
+    answer: "On this site dude comeon",
+    height: ["40px"],
   },
   {
-    question: 'Why mint?',
+    question: "Why mint?",
     answer:
       "You see the day we announce we're coming is the day 1ETH knocks off pudgy penguins floor, now we don't have to remind you where that market cap goes to",
-    height: ['150px', '80px', '60px']
+    height: ["150px", "80px", "60px"],
   },
   {
-    question: 'Who you?',
+    question: "Who you?",
     answer:
-      'Developers, NFT enthusiasts & Artists from Bhiwani, Rohtak, Delhi, Jhajjar - India',
-    height: ['90px', '50px', '40px']
+      "Developers, NFT enthusiasts & Artists from Bhiwani, Rohtak, Delhi, Jhajjar - India",
+    height: ["90px", "50px", "40px"],
   },
   {
-    question: 'Any charity?',
+    question: "Any charity?",
     answer: `We'll teach kids some coding skills & some communication & leadership skills in our villages,
     that's our charity - realworld impact. Also help a 60yr old dadi (granny) to make a crypto project, maybe on solana or Ada,
     most likely ETH- promote ease of crypto to our soil fam- building up a tech hub in our area would be cool`,
-    height: ['260px', '120px', '72px']
-  }
-]
+    height: ["260px", "120px", "72px"],
+  },
+];
 
 export const BACK_STORY = `Year 3000, wwwait we gotta have a recap. All the ice caps melted in 2100,
 all pudgy party PC penguins died in that one. We got hit by an icy meteor in 2400,
 all the apes, cats, dogs died in that one. Moon left rather quickly around 2700, aliens did that.
 Slowly earth's trajectory shifted away & planet cooled,
-it's all a water world now & only polar penguins remain`
+it's all a water world now & only polar penguins remain`;
